9c288d7d64d44149e7067f3ec288649572f7bd57
hiteshzoft <147499794+hiteshzoft@users.noreply.github.com>
2023-11-16 23:41:27 +0530

    Added On_Search

--- BaseController.ts
+++ BaseController.ts
@@ -2,4 +2,5 @@ import { NextFunction, Request, Response } from "express";
 
 export abstract class BaseController {
   abstract search(req: Request, res: Response, next: NextFunction): void;
+  abstract on_search(req: Request, res: Response, next: NextFunction): void;
 }
